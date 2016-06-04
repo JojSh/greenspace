eb001125caddb2f416d7dd493c1567fda64d0d60
Jojo <joseph.sharratt@gmail.com>
2016-06-04 13:55:23 +0100

    Pass first test

---
+++ app/controllers/greenspaces_controller.rb
@@ -0,0 +1,7 @@
+class GreenspacesController < ApplicationController
+
+  def index
+
+  end
+
+end
--- spec/features/greenspaces_feature_spec.rb
+++ spec/features/greenspaces_feature_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature 'greenspaces' do
-  context 'no greenspaces have been added' do
+  context 'none have been added yet' do
     scenario 'should display a prompt to add a greenspace' do
       visit '/greenspaces'
       expect(page).to have_content 'No greespaces added yet'
