b4c1ea3ee91df8ed4c7ffd385d3d48c4bb428a1c
Jojo <joseph.sharratt@gmail.com>
2016-06-07 17:26:13 +0100

    Move reviews and ratings to greenspace individual views

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -6,10 +6,13 @@ feature 'Reviewing' do
   scenario 'user can complete a form to leave a review' do
 
     visit '/greenspaces'
+    click_link 'Visit Richmond Park'
     click_link 'Review Richmond Park'
+    # click_button 'Review Richmond Park'
     fill_in "Thoughts", with: "Best park ever!"
     select '5', from: 'Rating'
     click_button 'Leave Review'
+    click_link 'Visit Richmond Park'
 
     # expect(current_path).to eq '/greenspaces'
     expect(page).to have_content 'Best park ever!'
